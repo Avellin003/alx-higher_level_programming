87910fb24ae9414607bc547ad14cf3a610492c38
Avellin <uavellinbonaparte@gmail.com>
2023-10-05 12:27:03 +0200

    comits

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,8 +5,8 @@ if __name__ == "__main__":
     if (a == 0):
         print("0 arguments.")
     elif (a == 1):
-        print(a, "argument: ")
+        print(a, "argument:")
     else:
-        print(a, "arguments: ")
+        print(a, "arguments:")
         for i in range(a):
             print("{}:".format(i + 1), (sys.argv[i + 1]))
