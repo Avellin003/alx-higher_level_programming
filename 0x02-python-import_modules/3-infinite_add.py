55d6370d90d31de6e0922b13f81ee0bc3321f136
Avellin <uavellinbonaparte@gmail.com>
2023-10-05 12:16:17 +0200

    updating

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-from sys import argv
-total = 0
-a = len(argv)
-for i in range(1, a):
-    total += int(argv[i])
-else:
-    print(total)
+if __name__ == "__main__":
+    from sys import argv
+    total = 0
+    a = len(argv)
+    for i in range(1, a):
+        total += int(argv[i])
+    else:
+        print(total)
