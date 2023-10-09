12ab119be99fe61d41f73d2c4a98cc903c606e27
Avellin <uavellinbonaparte@gmail.com>
2023-10-09 10:40:39 +0200

    updates

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,10 +2,10 @@
 def max_integer(my_list=[]):
     if not my_list:
         return None
-    max_value = my_list[0]
+    value = my_list[0]
     for i in range(1, len(my_list)):
-        if max_value < my_list[i]:
-            max_value = my_list[i]
+        if value < my_list[i]:
+            value = my_list[i]
         else:
             continue
-        return max_value
+        return value
