79a2a177c7f22961ddce55584dfa2c5745ff99a9
Avellin <uavellinbonaparte@gmail.com>
2023-10-09 10:27:12 +0200

    8-multiple_returns.py

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     if not sentence:
-        return None
+        length = len(sentence)
+        first = None
+        return (length, first)
     else:
         length = len(sentence)
         first = sentence[0]
