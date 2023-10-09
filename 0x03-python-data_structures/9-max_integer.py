c300ba33b15c4a2c34b97827d4977ede856beebf
Avellin <uavellinbonaparte@gmail.com>
2023-10-09 10:50:27 +0200

    9-max_integer.py

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -3,6 +3,6 @@ def max_integer(my_list=[]):
     if not my_list:
         return None
     else:
-#we use .sort as it writes in ascending order
+        # we use '.sort' as it writes in ascending order
         my_list.sort()
         return my_list[-1]
