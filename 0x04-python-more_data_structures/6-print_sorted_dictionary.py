42dcabe1e1edbe29f022ec08e3f82ea94252310b
Avellin <uavellinbonaparte@gmail.com>
2023-10-11 13:26:58 +0200

    nw

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -3,4 +3,4 @@ def print_sorted_dictionary(a_dictionary):
     new = list(a_dictionary.keys())
     new.sort()
     for i in new:
-        print("{}: {}".format(i,a_dictionary.get(i)))
+        print("{}: {}".format(i, a_dictionary.get(i)))
