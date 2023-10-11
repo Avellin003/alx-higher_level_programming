4623705660e54333494c903fc96b7576a64e71bd
Avellin <uavellinbonaparte@gmail.com>
2023-10-11 12:39:30 +0200

    1-search_replace.py

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     new_list = my_list.copy()
-    a = new_list.index(search)
-    new_list[a] = replace
-    return (new_list)
+    if search != replace:
+        a = new_list.index(search)
+        new_list[a] = replace
+        return(new_list)
+    else:
+        return (my_list)
