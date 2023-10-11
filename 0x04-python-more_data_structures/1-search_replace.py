298af0e8668c5af97fa9a97caf1a009a620c1dc8
Avellin <uavellinbonaparte@gmail.com>
2023-10-11 12:43:59 +0200

    updates

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    new_list = my_list.copy()
-    if search != replace:
-        a = new_list.index(search)
-        new_list[a] = replace
-        return(new_list)
-    else:
-        return (my_list)
+    new = list(map(lambda a: replace if a == search else a, my_list))
+    return new
