48d95d6b5cad47c6c2a94a2b84c1cdc621849cd0
Avellin <uavellinbonaparte@gmail.com>
2023-10-23 18:57:07 +0200

    101-safe_function.py

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import sys
+
+def safe_function(fct, *args):
+    try:
+        answer = fct(*args)
+    except Exception as avellin:
+        print("Exception: {}".format(avellin), file=sys.stderr)
+        return (None)
+    else:
+        return (answer)
