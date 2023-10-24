adda96c5205717e4562f567c4695793b17da15bf
Avellin <uavellinbonaparte@gmail.com>
2023-10-24 17:43:24 +0200

    new_updates

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -3,9 +3,7 @@ class Square:
     """
     Square module maker
     """
-
     def __init__(self, y):
         """
-        get's value y
-        """
+        get's value y"""
         self.__size = y
