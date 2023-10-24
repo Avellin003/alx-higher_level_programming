91675a880360daaae65ed03a6ec6a3a06b5486e3
Avellin <uavellinbonaparte@gmail.com>
2023-10-24 18:34:38 +0200

    new

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -6,6 +6,7 @@
 class Square:
 
     def __init__(self, size):
+
         """init square"""
 
         self.size = size
@@ -17,7 +18,9 @@ class Square:
 
     @size.setter
     def size(self, value):
+
         """check for errors and raise em"""
+
         if type(value) is not int:
             raise TypeError("size must be an integer")
         elif value < 0:
@@ -26,10 +29,13 @@ class Square:
             self.__size = value
 
     def area(self):
+
         """squares"""
+
         return (self.__size ** 2)
 
     def my_print(self):
+
         """function that prints '#'"""
 
         for i in range(0, self.__size):
