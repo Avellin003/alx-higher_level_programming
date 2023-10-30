d01824863bf08cdbdaa1480188500617bab79101
Avellin <uavellinbonaparte@gmail.com>
2023-10-30 22:16:40 +0200

    6-rectangle.py

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -6,7 +6,7 @@ class Rectangle:
     '''empty class'''
 
     number_of_instances = 0
- 
+
     def __init__(self, width=0, height=0):
         '''initializing the instances in class'''
         self.width = width
