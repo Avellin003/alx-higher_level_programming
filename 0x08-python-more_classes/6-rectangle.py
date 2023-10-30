d6382dfdc8a80b364007d786b9bee237e1faca19
Avellin <uavellinbonaparte@gmail.com>
2023-10-30 22:15:55 +0200

    6-rectangle.py

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -4,18 +4,19 @@
 
 class Rectangle:
     '''empty class'''
-    x_instance = 0
+
+    number_of_instances = 0
  
     def __init__(self, width=0, height=0):
         '''initializing the instances in class'''
         self.width = width
         self.height = height
-        Rectangle.x_instance += 1
+        Rectangle.number_of_instances += 1
 
     def __del__(self):
         '''prints string to be deleted'''
         print("Bye rectangle...")
-        Rectangle.x_instance -= 1
+        Rectangle.number_of_instances -= 1
 
     @property
     def width(self):
