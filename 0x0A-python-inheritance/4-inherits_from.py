f48fd7f8a51fd4054c98b3f71844221d95ca13d3
Avellin <uavellinbonaparte@gmail.com>
2023-11-06 14:49:27 +0200

    4-inherits_from.py

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -3,6 +3,7 @@
 write a function that returns true subclass else false
 '''
 
+
 def inherits_from(obj, a_class):
     '''checks if it's subclass'''
     return(issubclass(type(obj), a_class) and type(obj) != a_class)
