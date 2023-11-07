a3f302cf3016c8a2282eae385c7e2b6c9f787b2a
Avellin <uavellinbonaparte@gmail.com>
2023-11-07 19:18:25 +0200

    updates

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -6,4 +6,4 @@ import json
 def save_to_json_file(my_obj, filename):
     """function hta saves to json files"""
     with open(filename, "w", encoding="utf-8") as a:
-        json.dump(my_obj. a)
+        json.dump(my_obj, a)
