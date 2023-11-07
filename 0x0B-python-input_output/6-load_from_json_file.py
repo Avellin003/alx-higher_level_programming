79fe25a5ba92154a4876b9990649abdfcd945743
Avellin <uavellinbonaparte@gmail.com>
2023-11-07 19:27:10 +0200

    6-load_from_json_file.py

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -5,5 +5,6 @@ import json
 
 def load_from_json_file(filename):
     """function that loads from json file"""
-    with open(filname) as a:
-        return json.load(a)
+    with open(filname, "r") as a:
+        data = json.load(a)
+        return data
