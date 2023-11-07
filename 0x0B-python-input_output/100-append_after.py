23d0f538baef7cfc1487cfa6934fa90ef5d3d9c7
Avellin <uavellinbonaparte@gmail.com>
2023-11-07 21:31:13 +0200

    100-append_after.py

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -7,11 +7,11 @@ def append_after(filename="", search_string="", new_string=""):
     with open(filename, "r", encoding="utf-8") as a:
         listl = []
         while True:
-            l = a.readline()
-            if l == "":
+            ls = a.readline()
+            if ls == "":
                 break
             listl.append(l)
-            if search_string in l:
+            if search_string in ls:
                 listl.append(new_string)
     with open(filename, "w", encoding="utf-8") as b:
         b.writelines(listl)
