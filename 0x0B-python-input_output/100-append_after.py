76889adc56ed570ad60d39106cb891f7e1cddd67
Avellin <uavellinbonaparte@gmail.com>
2023-11-07 20:31:53 +0200

    100-append_after.py

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -5,13 +5,13 @@
 def append_after(filename="", search_string="", new_string=""):
     """creation"""
     with open(filename, "r", encoding="utf-8") as a:
-        li = []
+        listl = []
         while True:
             l = a.readline()
             if l == "":
                 break
-            l.append(li)
+            listl.append(l)
             if search_string in l:
                 li.append(new_string)
     with open(filename, "w", encoding="utf-8") as b:
-        b.writelines(li)
+        b.writelines(listl)
