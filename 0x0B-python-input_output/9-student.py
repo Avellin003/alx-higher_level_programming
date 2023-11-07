cd21e46bf612d495413fc0cce149fc645819a3c2
Avellin <uavellinbonaparte@gmail.com>
2023-11-07 19:54:52 +0200

    9-student.py

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""creation of class student"""
+
+
+class Student:
+    """public attributes of student"""
+    def __init__(self, first_name, las_name, age):
+        """init student"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+    
+    def to_json(self):
+        """the public method"""
+        return (self.__dict__)
