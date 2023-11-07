9356b80ad8b5b7759069aa1f509e1eaf62ce2fb3
Avellin <uavellinbonaparte@gmail.com>
2023-11-07 19:36:00 +0200

    7-add_item.py

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""write script that adds to python list"""
+from sys import argv as ar
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+
+filename = "add_item.json"
+
+try:
+    json_list = load_from_json(filename)
+except FileNotfoundError:
+    json_list = []
+
+for i in ar[1:]:
+    json_list.append(ar)
