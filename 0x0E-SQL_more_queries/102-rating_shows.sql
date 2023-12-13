9d444487a7a3825aa2807fa2c9be98e70f5bb605
Avellin003 <uavellinbonaparte@gmail.com>
2023-12-13 04:53:27 -0800

    updates

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,7 +1,7 @@
 -- 102_rating_shows.sql
 SELECT `title`, SUM (`rate`) AS `rating`
- FROM `tv_shows` AS a
-	INNER JOIN `tv_show_ratings` AS b
-	ON a.`id` = b.`show_id`
+ FROM `tv_shows` AS t
+	INNER JOIN `tv_show_ratings` AS r
+	ON t.`id` = r.`show_id`
 GROUP BY `title`
 ORDER BY `rating` DESC;
