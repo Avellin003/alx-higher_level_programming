bb8d5b72c200a16ba287b2274e5ea7e87569212e
Avellin003 <uavellinbonaparte@gmail.com>
2023-12-13 05:16:16 -0800

    updates

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -1,17 +1,17 @@
 -- show title
 SELECT DISTINCT `title`
- FROM `tv_shows` AS A
+ FROM `tv_shows` AS t
 	LEFT JOIN `tv_show_genres` as B
-	ON B.`show_id` = A.`id`
+	ON B.`show_id` = t.`id`
 	LEFT JOIN `tv_genres` AS C
 	ON C.`id` = B.`genre_id`
 	WHERE t.`title` NOT IN
 		(SELECT `title`
-			FROM `tv_shows` AS A
-				INNER JOIN `tv_show_genres` AS B
-				ON B.`show_id` = A.`id`
-				INNER JOIN `tv_genres` AS C
-				ON C.`id` = B.`genre_id`
-				WHERE C.`genre_id`
-				WHERE C.`name` = "Comedy")
+		   FROM `tv_shows` AS t
+			INNER JOIN `tv_show_genres` AS B
+			ON B.`show_id` = t.`id`
+			INNER JOIN `tv_genres` AS C
+			ON C.`id` = B.`genre_id`
+			WHERE C.`genre_id`
+			WHERE C.`name` = "Comedy")
 ORDER BY `title`;
