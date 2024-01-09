9a8492ac947870e36d486a72e465e1c27fc2fd01
root <root@avellin.localdomain>
2024-01-09 14:44:41 +0200

    updates

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -2,7 +2,7 @@
 let i = 0;
 const num = process.argv[2];
 if (typeof num !== 'number') {
- console.log('Missing number of occurence');
+  console.log('Missing number of occurence');
 } while (i < num) {
   console.log('C is fun');
   i++;
