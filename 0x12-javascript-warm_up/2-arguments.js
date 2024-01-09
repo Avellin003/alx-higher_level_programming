2e1ef032856d88b969a198e3141fc7dcedaa6b13
root <root@avellin.localdomain>
2024-01-09 13:50:06 +0200

    updates

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,14 +1,9 @@
 #!/usr/bin/node
 const i = process.argv.length - 2;
-if (i === 0)
-{
-	console.log('No argument');
-}
-else if (i === 1)
-{
-	console.log('Argument found');
-}
-else
-{
-	console.log('Arguments found');
+if (i === 0) {
+  console.log('No argument');
+} else if (i === 1) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }
