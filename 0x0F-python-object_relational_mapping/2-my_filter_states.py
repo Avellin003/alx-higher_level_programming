3d26a49262e2e9496444cc9de5c394a477b1bda8
root <root@avellin.localdomain>
2024-01-13 11:49:41 +0200

    updates

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -10,8 +10,7 @@ if __name__ == "__main__":
             user=sys.argv[1],
             passwd=sys.argv[2],
             db=sys.argv[3],
-            port=3306
-            )
+            port=3306)
     cr = db.cursor()
     cr.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'"
             .format(sys.argv[4]))
