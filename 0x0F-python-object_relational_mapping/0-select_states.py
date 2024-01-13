d64e67b314d2fd9b059489d9945408bb0f94c212
root <root@avellin.localdomain>
2024-01-13 10:04:49 +0200

    updates

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""creation of a script tha lists all states"""
+
+if __name__ == "__name__":
+
+    import MySQLdb
+    import sys
+
+    connect = MySQL.connect(
+            host='localhost',
+            port=3306,
+            user=f'{sys.argv[1]}',
+            passwd=sys.argv[2],
+            db=f'{sys.argv[3]}',
+            charset='utf8'
+            )
+    sql = db.cursor()
+    sql.execute('SELECT * FROM states ORDER BY id ASC')
+    table = sql.fetchall()
+    for i in table:
+        print(i)
+    sql.close()
+    connect.close()
