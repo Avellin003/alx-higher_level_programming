b27f19a95e6fda5e401d4d0b0216e6a1d291d0b0
root <root@avellin.localdomain>
2024-01-13 11:45:45 +0200

    updates

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -3,6 +3,7 @@
 import MySQLdb
 import sys
 
+
 if __name__ == "__main__":
     db = MySQLdb.connect(
             host="localhost",
@@ -11,7 +12,6 @@ if __name__ == "__main__":
             db=sys.argv[3],
             port=3306
             )
-
     cr = db.cursor()
     cr.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'"
             .format(sys.argv[4]))
