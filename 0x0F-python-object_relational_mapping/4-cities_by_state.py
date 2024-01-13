881aadc3931e1d773dae815c58d3d4bbeb1c3a46
root <root@avellin.localdomain>
2024-01-13 17:17:22 +0200

    updsteds

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -5,8 +5,12 @@ import sys
 
 
 if __name__ = "__main__":
-    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
-            passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    db = MySQLdb.connect(
+            host="localhost",
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3],
+            port=3306)
     cr = db.cursor()
     cr.execute("""SELECT cities.id, cities.name, states.name FROM
     citie INNER JOIN states ON  states.id=cities.state_id""")
