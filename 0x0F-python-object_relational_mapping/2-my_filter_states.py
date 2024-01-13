26293e98ad67d51bff3bd22b9688b8431a62a4b1
root <root@avellin.localdomain>
2024-01-13 17:44:26 +0200

    updates

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -7,10 +7,10 @@ if __name__ == "__main__":
     db = MySQLdb.connect(
             host="localhost",
             user=sys.argv[1],
+            port=3306,
             passwd=sys.argv[2],
-            db=sys.argv[3],
             charset='utf8',
-            port=3306
+            db=sys.argv[3]
             )
     cr = db.cursor()
     cr.execute("SELECT * FROM states\
