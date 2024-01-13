a894fae061a7d106e5d45effa333ac2cd70db8c9
root <root@avellin.localdomain>
2024-01-13 17:38:26 +0200

    updates

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python3
-
 """create MySQLdb"""
 
-import MySQLdb
-import sys
-
-
 if __name__ == "__main__":
+    import MySQLdb
+    import sys
+
     db = MySQLdb.connect(
             host="localhost",
             user=sys.argv[1],
             passwd=sys.argv[2],
             db=sys.argv[3],
+            charset='utf8',
             port=3306)
     cr = db.cursor()
-    cr.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'"
-            .format(sys.argv[4]))
+    cr.execute("SELECT * FROM states\
+            WHERE name = '{}'\
+            ORDER BY id ASC".format(sys.argv[4]))
     tables = cr.fetchall()
     for i in tables:
         print(i)
