ab4e045dcdd82220b910ad1b283d11131befe0e9
root <root@avellin.localdomain>
2024-01-13 11:27:03 +0200

    updates

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -4,7 +4,7 @@ import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    db = MySQL.connect(
+    db = MySQLdb.connect(
             host="localhost",
             user=sys.argv[1],
             passwd=sys.argv[2],
