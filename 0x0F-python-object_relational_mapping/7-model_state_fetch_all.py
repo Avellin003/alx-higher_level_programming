378cd971ad702e11b570590a71ed65a5c6078d8d
root <root@avellin.localdomain>
2024-01-14 06:40:32 +0200

    updates

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]))
     Base.metadata.create_all(engine)
-    Ses = sessionmaker(bind=engine)
-    session = Ses()
+    Session = sessionmaker(bind=engine)
+    session = Session()
     for instance in session.query(State).order_by(State.id):
         print(instance.id, instance.name, sep=": ")
