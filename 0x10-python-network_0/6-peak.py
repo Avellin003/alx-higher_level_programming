5457102e4e084a13d8eb40def7c5697faa9a3f65
root <root@avellin.localdomain>
2024-01-25 20:17:42 +0200

    updates

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,30 +1,28 @@
 #!/usr/bin/python3
-"""This function finds the peak number"""
+"""function that performs peak finding algorithm"""
+
 
 def find_peak(list_of_integers):
-    """
-    Find a peak in a list of unsorted integers.
-
-    Args:
-    - list_of_integers: List of unsorted integers.
-
-    Returns:
-    - The peak element.
-
-    Note: There may be more than one peak in the list.
-    """
-    left, right = 0, len(list_of_integers - 1)
-
-    if not list_of_integers:
+    """finds the peak """
+    if list_of_integers == []:
         return None
 
-    while left < right:
-        mid = (left + right) // 2
+    lenth = len(list_of_integers)
+    mid = int(lenth / 2)
+    lis = list_of_integers
 
-        if list_of_integers[mid] < list_of_integers[mid + 1]:
-            left = mid + 1
+    if mid - 1 < 0 and mid + 1 >= lenth:
+        return lis[mid]
 
-        else:
-            right = mid
+    elif mid - 1 < 0:
+        return lis[mid] if lis[mid] > lis[mid + 1] else lis[mid + 1]
 
-    return list_of_integers[left]
+    elif mid + 1 >= lenth:
+        return lis[mid] if lis[mid] > lis[mid - 1] else lis[mid - 1]
+
+    if lis[mid - 1] < lis[mid] > lis[mid + 1]:
+        return lis[mid]
+
+    if lis[mid + 1] > lis[mid - 1]:
+        return find_peak(lis[mid:])
+    return find_peak(lis[:mid])
