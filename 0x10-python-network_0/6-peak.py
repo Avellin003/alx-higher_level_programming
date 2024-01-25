6ae98cfba2355bbcf22c7265509b9580cadebfc2
root <root@avellin.localdomain>
2024-01-25 16:35:52 +0200

    updates

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+"""This function finds the peak number"""
+
 def find_peak(list_of_integers):
     """
     Find a peak in a list of unsorted integers.
@@ -11,17 +13,17 @@ def find_peak(list_of_integers):
 
     Note: There may be more than one peak in the list.
     """
+    left, right = 0, len(list_of_integers - 1)
 
     if not list_of_integers:
         return None
-
-    left, right = 0, len(list_of_integers) - 1
 
     while left < right:
         mid = (left + right) // 2
 
         if list_of_integers[mid] < list_of_integers[mid + 1]:
             left = mid + 1
+
         else:
             right = mid
 
