70533eb1d1cbe611185cee6ed531ccdd21b898f2
root <root@avellin.localdomain>
2024-01-26 14:02:58 +0200

    updates

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -3,11 +3,12 @@
 import sys
 import requests
 
+
 if __name__ == "__main__":
     url = sys.argv[1]
 
     req = requests.get(url)
     if req.status_code >= 400:
-        print("Error code: {}"format(req.status_code))
+        print("Error code: {}".format(req.status_code))
     else:
         print(req.text)
