86e852ba29a2cab0bddaec36a85a813c0d661719
root <root@avellin.localdomain>
2024-01-26 13:40:36 +0200

    updates

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -6,6 +6,6 @@ import requests
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    val = {"email": sys.argv[2]}
-    req = requests.post(url, data=val)
-    print(req.text))
+    req = urllib.request.Request(url)
+    with urllib.request.urlopen(req) as ans:
+        print(dict(ans.headers).get("X-Request-Id"))
