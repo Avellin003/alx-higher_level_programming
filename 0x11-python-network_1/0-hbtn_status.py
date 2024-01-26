ab917154ac8a3d79836f1f182dc98d160198b9a8
root <root@avellin.localdomain>
2024-01-26 12:26:02 +0200

    p

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,8 +3,7 @@
 import urllib.request
 
 if __name__ == "__main__":
-    url = "https://alx-intranet.hbtn.io/status"
-    req = urllib.request.Request(url)
+    req = urllib.request.Request("https://alx-intranet.hbtn.io/status")
     with urllib.request.urlopen(req) as response:
         content = response.read()
         print("Body response:")
