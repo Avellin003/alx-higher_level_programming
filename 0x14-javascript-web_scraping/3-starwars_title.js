4e820a80299e72011271bccb755b383bab16cdef
root <root@avellin.localdomain>
2024-02-20 15:45:58 +0200

    updaes

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -2,5 +2,5 @@
 const req = require('request');
 const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
 req(url, function (error, response, body){
-	console.log(error || JSON.parse(body).title);
+  console.log(error || JSON.parse(body).title);
 });
